29394e09fcc8451d15877240dd02d3aea2f718ff
NovaAnte <andreas_cabraja@hotmail.com>
2022-10-13 23:34:38 +0200

    Fixed and simplified the code for checking if a letter is on the correct position.

--- src/views/SolveView.js
+++ src/views/SolveView.js
@@ -6,6 +6,7 @@ const SolveView = () => {
 
     const [arr, setArr] = useState(["aback", "abase", "abate", "abbey", "abbot"])
     const [inputWord, setInputWord] = useState('')
+    let greenCount = 0
 
     const handleClick = () => {
         const charArr = inputWord.split("");
@@ -18,10 +19,13 @@ const SolveView = () => {
                 valueArr.push({letter: charArr[i], color: 'orange'})
             } else if (charArr[i-1] === '+'){
                 valueArr.push({letter: charArr[i], color: 'green'})
+                greenCount++;
             } else if (charArr[i-1] !== '+' && charArr[i-1] !== '-'){
                 valueArr.push({letter: charArr[i], color: 'blank'})
             }
         }
+
+        console.log(greenCount)
 
         for (let i = 0; i < valueArr.length; i++) {
             console.log(valueArr[i])
@@ -29,26 +33,49 @@ const SolveView = () => {
 
         let tempArr = []
 
+       // for (let i = 0; i < valueArr.length; i++) { // loopa genom alla bokstäver i input
+       //      if (valueArr[i].color === 'green'){
+       //           for (let j = 0; j < arr.length; j++) { // loopa genom alla ord i listan
+       //              let loopGreenCount = 0
+       //              const splitWord = arr[j].split('')
+       //               for (let k = 0; k < splitWord.length; k++) { // loopa genom alla bokstäver i ett ord
+       //                      if (splitWord.indexOf(splitWord[k]) === valueArr.indexOf(valueArr[i])){ // om en bokstav i ordet matchar en grön bokstav, lägg till den till arrayen. MEN om vi har flera gröna?
+       //                          loopGreenCount++;
+       //
+       //                          console.log('loopgreencount', splitWord, loopGreenCount)
+       //
+       //                          if (greenCount === loopGreenCount){
+       //                              tempArr.push(arr[j]);
+       //                          }
+       //                      }
+       //
+       //              }
+       //          }
+       //      }
+       //  }
 
-        for (let i = 0; i < valueArr.length; i++) {
-            if (valueArr[i].color === 'green'){
-                for (let j = 0; j < arr.length; j++) {
-                    const splitWord = arr[j].split('')
-                    for (let k = 0; k < splitWord.length; k++) {
-                        if (splitWord[k] === valueArr[i].letter){
-                            if (splitWord.indexOf(splitWord[k]) === valueArr.indexOf(valueArr[i])){
-                                console.log(splitWord.indexOf(splitWord[k]))
-                                console.log(valueArr.indexOf(valueArr[i]))
-                                tempArr.push(arr[j]);
-                                break;
-                            }
 
-                        }
+        for (let i = 0; i < arr.length; i++) { // loopa genom alla ord i listan
+            let greenLoop = 0
+            const splitWord = arr[i].split('') // splitta alla ord i listan
+            for (let j = 0; j < splitWord.length; j++) { // för varje bokstav per ord i listan
+                if (valueArr[j].color === 'green'){
+                    if (splitWord[j] === valueArr[j].letter){
+                        greenLoop++;
+                    }
+                    if (greenLoop === greenCount){
+                        tempArr.push(arr[i])
                     }
                 }
+
             }
         }
+
+
+        setArr(tempArr)
         console.log(tempArr)
+
+
 
 
 
