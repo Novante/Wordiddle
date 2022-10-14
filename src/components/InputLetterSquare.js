ab3de3005885ff2f7d2f313fe7acff7ed03127d6
NovaAnte <andreas_cabraja@hotmail.com>
2022-10-14 13:15:49 +0200

    Added logic for switching backgroundcolor of each button.

--- src/components/InputLetterSquare.js
+++ src/components/InputLetterSquare.js
@@ -1,18 +1,41 @@
+import {useState} from "react";
+
 const InputLetterSquare = ({letter}) => {
-    return (<>
-        <button style={styles.container}>
-            {letter}
+
+    const [backgroundColor, setBackgroundColor] = useState('white')
+    const [buttonCount, setButtonCount] = useState(0)
+
+    const changeColor = () => {
+        if (buttonCount === 2){
+            setButtonCount(0)
+        } else {
+            setButtonCount(buttonCount + 1)
+        }
+
+        if (buttonCount === 0){
+            setBackgroundColor('white')
+        } else if (buttonCount === 1){
+            setBackgroundColor('orange')
+        } else if (buttonCount === 2){
+            setBackgroundColor('green')
+        }
+    }
+
+    return (
+        <>
+        <button onClick={changeColor} style={{...styles.container, backgroundColor: backgroundColor}}>
+            {letter.toUpperCase()}
         </button>
     </>)
 }
 
-const styles = {
+let styles = {
     container: {
         border: '1px solid black',
-        width: '20px',
+        width: '50px',
+        height: '50px',
         textAlign: 'center',
-        margin: '5px'
-
+        margin: '5px',
     }
 }
 export default InputLetterSquare
