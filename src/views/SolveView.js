d65e00a36b8ece8bd48cb93c1b628d485f323ff2
Novante <78790665+Novante@users.noreply.github.com>
2022-10-28 17:47:53 +0200

    Yet another fix

--- src/views/SolveView.js
+++ src/views/SolveView.js
@@ -348,31 +348,34 @@ const SolveView = () => {
             }
 
 
-            let joinedGOArr = greenAndOrangeArr.join('')
+if (equalOrangeLettersArr.length > 0){
+    let joinedGOArr = greenAndOrangeArr.join('')
 
-            for (let e = 0; e < 5; e++) {
-                for (let i = 0; i < tempArr.length; i++) {
-                    // console.log(i)
-                    let joinedGOArrCount = joinedGOArr.split(valueArr[e]?.letter).length - 1
-                    console.log(joinedGOArrCount)
+    for (let e = 0; e < 5; e++) {
+        for (let i = 0; i < tempArr.length; i++) {
+            // console.log(i)
+            let joinedGOArrCount = joinedGOArr.split(valueArr[e]?.letter).length - 1
+            console.log(joinedGOArrCount)
 
-                    let ct = tempArr[i].split(valueArr[e]?.letter).length - 1  // green in word
+            let ct = tempArr[i].split(valueArr[e]?.letter).length - 1  // green in word
 
-                    if (ct >= joinedGOArrCount && (valueArr[e]?.color === 'orange' || valueArr[e]?.color === 'green')){
-                        console.log('green in target' + ct)
-                        console.log('green in source' + joinedGOArrCount)
-                        console.log('target word: ' + tempArr[i])
-                        finalDeleteArr.push(tempArr[i])
-                    }
-
-
-                }
+            if (ct >= joinedGOArrCount && (valueArr[e]?.color === 'orange' || valueArr[e]?.color === 'green')){
+                console.log('green in target' + ct)
+                console.log('green in source' + joinedGOArrCount)
+                console.log('target word: ' + tempArr[i])
+                finalDeleteArr.push(tempArr[i])
             }
+
+
+        }
+    }
+
+
 
             tempArr = tempArr.filter((word) => finalDeleteArr.includes(word))
             console.log(finalDeleteArr)
             console.log(tempArr)
-
+}
             setArr(tempArr)
 
 
